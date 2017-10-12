f7564912a6837ba98ee3fa2d60d5032cbbcec01c
kazuki1402 <kazuki1402@i.softbank.jp>
2017-10-12 12:36:50 +0900

    テストコミット
    製作者:中谷一貴

--- wizard/Assets/Scripts/Player/PlayerState.cs
+++ wizard/Assets/Scripts/Player/PlayerState.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerState : PlayerBase {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
 }
