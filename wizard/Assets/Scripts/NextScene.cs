04e69f3bc9345406b5786e1ffc96344d1e0940ca
TSITCL\s152189 <fkei120@yahoo.co.jp>
2017-10-13 10:32:55 +0900

    シーン移動

---
+++ wizard/Assets/Scripts/NextScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+
+    public enum GameScene
+    {
+        Title,
+        Select,
+        MagicSelect,
+        Play,
+        Result
+    }
+
+    [SerializeField]
+    private GameScene nextScene;
+
+	// Use this for initialization
+	public void GoToScene() {
+
+        SceneManager.LoadScene(nextScene.ToString());
+    }
+
+}
