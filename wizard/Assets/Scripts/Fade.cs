b7e9bc905f3377a20721b7e516a5fd6b4226f1d5
TSITCL\s152189 <fkei120@yahoo.co.jp>
2017-10-13 14:54:39 +0900

    タイトルの追加

---
+++ wizard/Assets/Scripts/Fade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+    [SerializeField]
+    private Image image;
+
+    bool flag = true;
+
+    Color alpha = new Color(0, 0, 0, 0.01f);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(image.color.a <= 0)
+        {
+            flag = true;
+        }
+        if(image.color.a >= 1.0f)
+        {
+            flag = false;
+        }
+
+
+        if (flag)
+        {
+            image.color += alpha;
+        }
+        else
+        {
+            image.color -= alpha;
+        }
+
+    }
+}
